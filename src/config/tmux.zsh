d186171569a5df14b1bd7037a660bdda51a7a102
wuchuheng <root@wuchuheng.com>
2024-02-02 20:52:19 +0800

    feat(config/tmux): Conditional tmux Configuration
    - Updated `tmux.zsh` in `src/config`:
       - Implemented a check to determine if `tmux` command exists before applying configuration settings.
       - Conditionally set the default terminal to "tmux-256color" only if tmux is available, enhancing compatibility and preventing errors in environments where tmux is not installed.
    
    This enhancement ensures that tmux-specific configurations are applied only in environments where tmux is present, improving the robustness and portability of the shell setup.

--- src/config/tmux.zsh
+++ src/config/tmux.zsh
@@ -1,3 +1,9 @@
 #!/usr/bin/env zsh 
 
-set -g default-terminal "tmux-256color"
+# if then cmd `tmux` exists, and then load the flowing configure.
+local hasTmux=$( whereis tmux )
+hasTmux=${hasTmux#tmux:}
+if [[ -n ${hasTmux} ]]; then
+  # Set the default terminal to tmux-256color
+  set -g default-terminal "tmux-256color"
+fi
