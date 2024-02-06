7558f4d49cc950ef468f1216364f8e21290ef5d1
wuchuheng <root@wuchuheng.com>
2024-02-06 17:48:55 +0800

    feat(workflows): Introduce New Workflow for AI-GPT Prompt with Git Staged Changes
    - Added a new workflow `git_stage_gdp_workflow` within `git-gdp.workflow.zsh` to enhance the interaction with AI GPT for generating standard git commit messages. This workflow focuses on utilizing the diff from the git staged cache, combining it with a predefined prompt to guide the AI in generating commit messages in a specific format.
    - The workflow extracts the staged changes using `git diff --staged`, appends a custom prompt detailing the desired commit message format, and copies the combined content to the clipboard. This approach aims to streamline the process of generating commit messages directly from staged changes, making it more efficient and aligned with AI GPT's capabilities.
    - Integrated actions to launch and activate Chrome's first tab, assuming the AI GPT interface is ready for input, facilitating a seamless transition from copying the diff and prompt to inputting it into the AI tool.
    
    This update introduces a practical tool for developers to leverage AI assistance in crafting commit messages that adhere to conventional formats, directly from their git staging area, enhancing productivity and consistency in commit documentation.

--- src/workflows/git-gdp.workflow.zsh
+++ src/workflows/git-gdp.workflow.zsh
@@ -4,6 +4,18 @@
 
 import ./actions/activate-chrome.action.zsh --as activate_chrome_action
 import ./actions/switch-chrome-tab1.action.zsh --as switch_chrome_tab1_action
+import ./git-gdp.workflow.zsh --as self
+
+function prompt() {
+    cat <<EOF
+please, show the me the standar git commit message for the above changes?
+and the git commit message must be used the prefix like:
+    feat: ... or test: ... or docs: ... etc.
+and try to add a option that can be used to add the scope of the changes,like:
+    feat(plugin): Add a new plugin ... or docs(conf): add a documents for some config ...
+    feat(workflows): ...
+EOF
+}
 
 # @Description: This workflow is used to generate the standard git commit message by using the AI gdp.
 # This is a workflow to do the flowing steps:
@@ -17,16 +29,32 @@ function git_gdp_workflow() {
     local content=$( cat  <<EOF
     $diff
     
-
-please, show the me the standar git commit message for the above changes?
-and the git commit message must be used the prefix like: 
-    feat: ... or test: ... or docs: ... etc.
-and try to add a option that can be used to add the scope of the changes,like:
-    feat(plugin): Add a new plugin ... or docs(conf): add a documents for some config ... 
-    feat(workflows): ...
+$(call self.prompt)
 EOF
 )
    print $content | pbcopy 
+   call activate_chrome_action.activate_chrome_action
+   call switch_chrome_tab1_action.switch_chrome_tab1_action
+}
+
+##
+# @Description: This workflow is used to generate the standard git commit message by using the AI gdp.
+# This is a workflow to do the flowing steps:
+# - show the changes
+# - add the prompt for the AI gdp to use the standard git commit message to generate the standar commit message
+# - copy the changes to the clipboard
+##
+function git_stage_gdp_workflow() {
+    local diff=$(git diff --staged )
+    # | pbcopy
+    local content=$( cat  <<EOF
+    $diff
+
+
+$(call self.prompt)
+EOF
+)
+   print $content | pbcopy
    call activate_chrome_action.activate_chrome_action
    call switch_chrome_tab1_action.switch_chrome_tab1_action
 }
