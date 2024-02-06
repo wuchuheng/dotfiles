098c5944780669f8e66049406277c0f27dd72976
wuchuheng <root@wuchuheng.com>
2024-02-06 16:18:59 +0800

    feat: add the proxy.unset function to unset the proxy in zsh

--- src/plugins/shell-proxy/shell-proxy.plugin.zsh
+++ src/plugins/shell-proxy/shell-proxy.plugin.zsh
@@ -11,6 +11,22 @@ function proxy() {
     proxy.set
 }
 
+function proxy.unset() {
+    unset http_proxy
+    unset https_proxy
+    unset ftp_proxy
+    unset no_proxy
+    unset HTTP_PROXY
+    unset HTTPS_PROXY
+    unset FTP_PROXY
+    unset NO_PROXY
+    # Unset the proxy in git.
+    git config --global --unset http.proxy
+    git config --global --unset https.proxy
+    # Unset the proxy in the shell.
+    call log.info "Unset the proxy in the shell." --no-path
+}
+
 if [[ ${SHELL_PROXY_ENABLED} == ${TRUE}  ]]; then
     proxy
 fi
