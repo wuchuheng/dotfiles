cc450c5307cf296e6cb74a747a6b8d664bc0a88e
wuchuheng <root@wuchuheng.com>
2024-02-08 02:06:20 +0800

    feat(plugin): add a new plugin: dns

---
+++ src/plugins/dns/dns.plugin.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh 
+
+##
+# This is a simple plugin to open the browser with the DNS lookup of the domain.
+# @Author: wuchuheng<root@wuchuheng.com>
+# @Date: 2024-02-08
+##
+
+function dns.a() {
+    # Ask the domain from the user in the terminal.
+    read "REPLY?Enter the domain: "
+    
+    local domain=$REPLY
+    local url="https://toolbox.googleapps.com/apps/dig/#A/${domain}"
+    printf "${url}\n"
+    # Open the browser with the URL.
+    open $url
+}
+
+function dns() {
+    dns.a
+}
