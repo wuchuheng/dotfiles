35b9b69fe796ca7066ba97281aee0cd95b20636c
wuchuheng <root@wuchuheng.com>
2024-02-08 02:13:52 +0800

    feat(plugin): add a new plugin named certificate

---
+++ src/plugins/certificate/certificate.plugin.zsh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+
+##
+# This is a simple plugin to create a new certificates from Let's Encrypt.
+# @Author: wuchuheng<root@wuchuheng.com>
+# @Date: 2024-02-08
+##
+
+##
+# Create a new certificate by DNS.
+##
+function certificate.create_by_dns() {
+    local certificateDir="/etc/letsencrypt"
+    sudo docker run -it --rm --name certbot \
+        -v "${certificateDir}:/etc/letsencrypt" \
+        -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+        certbot/certbot certonly  --manual --preferred-challenges dns
+    if [ $? -eq 0 ]; then
+        echo "The certificate has been created successfully and saved in ${certificateDir}"
+    fi
+}
