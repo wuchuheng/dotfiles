cea706d48080a67fc4a1d84367da53dc66960b46
wuchuheng <root@wuchuheng.com>
2024-02-08 03:39:39 +0800

    Add Docker Compose configuration and HTTP server launch script
    - Added `http-server.plugin.zsh` with function to start HTTP server using Docker.
    - Included certificates for localhost and `local.wuchuheng.com`.
    
    Resolves: #issue_number (if applicable)

---
+++ src/plugins/http-server/http-server.plugin.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+##
+# Lanch a http server.
+##
+function http.start() {
+    local currentDir="${G_DOTFILES_ROOT}/src/plugins/http-server"
+    docker run -it --rm --name http-server \
+        -p '80:80' \
+        -p '443:443' \
+        -v "${currentDir}/www/:/www" \
+        -v ${currentDir}/nginx/:/etc/nginx/ \
+        --name http-server \
+        nginx:latest
+
+      # - ./certs:/etc/nginx/certs
+}
