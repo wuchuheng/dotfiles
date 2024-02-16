8db181cc431d65684effc888cb2ed9e2945fc040
wuchuheng <root@wuchuheng.com>
2024-02-16 15:56:59 +0800

    feat(web-vim): Introduce web development environment setup with Neovim
    - Removed global `.prettierignore`, `.prettierrc.json`, `package.json`, and `pnpm-lock.yaml` files to streamline the project configuration, focusing on a more Neovim-centric development setup.
    - Introduced a dedicated `.vscode` configuration within the `web-vim` plugin directory, including `extensions.json` and `settings.json`, to support Lua formatting and JSON with Stylua and Prettier, enhancing the coding experience specifically for web development.
    - Deleted the initial `init.lua` configuration to pave the way for a more advanced setup using AstroNvim as the base for the Neovim environment, catering to web development needs.
    - Updated the `web-vim` plugin's launcher script to dynamically set XDG paths and clone AstroNvim if not already installed, ensuring a ready-to-use web development environment on launch.
    - Added `.gitignore` entries for `beta-vim` and updated entries for `web-vim`, maintaining a clean workspace by ignoring cache and data generated by Neovim plugins and configurations.
    
    This update refocuses the project's tooling on Neovim for web development, leveraging AstroNvim for an out-of-the-box enhanced developer experience while ensuring VS Code remains an integral part of the workflow with tailored settings for Lua and JSON.

--- src/plugins/web-vim/web-vim.plugin.zsh
+++ src/plugins/web-vim/web-vim.plugin.zsh
@@ -1,7 +1,28 @@
 #!/usr/bin/env zsh
 
 # This is the entry point for 
-
 function web-vim() {
-    XDG_CONFIG_HOME=${G_DOTFILES_ROOT}/src/plugins/web-vim/config nvim
+    local currentDir=${G_DOTFILES_ROOT}/src/plugins/web-vim
+    local XDG_DATA_HOME=${currentDir}/data;
+    [[ ! -d ${XDG_DATA_HOME} ]] && mkdir -p ${XDG_DATA_HOME}
+    local XDG_CACHE_HOME=${currentDir}/cache
+    [[ ! -d ${XDG_CACHE_HOME} ]] && mkdir ${XDG_CACHE_HOME}
+    local XDG_STATE_HOME=${currentDir}/state
+    [[ ! -d  ${XDG_STATE_HOME} ]] && mkdir -p ${XDG_STATE_HOME}
+    local XDG_CONFIG_HOME=${currentDir}/config
+    [[ ! -d ${XDG_CONFIG_HOME} ]] && mkdir -p ${XDG_CONFIG_HOME}
+    
+    # Check if the https://github.com/AstroNvim/AstroNvim was not installed under the path ${XDG_CONFIG_HOME}/nvim,
+    # and then try to install it.
+    local astroNvimInstalledPath=${XDG_CONFIG_HOME}/nvim
+    if [[ ! -d ${astroNvimInstalledPath} ]]; then
+        mkdir -p ${astroNvimInstalledPath}
+        git clone --depth 1 https://github.com/AstroNvim/AstroNvim ${astroNvimInstalledPath}
+    fi
+
+    XDG_CACHE_HOME=${XDG_CACHE_HOME} \
+    XDG_DATA_HOME=${XDG_DATA_HOME} \
+    XDG_STATE_HOME=${XDG_STATE_HOME} \
+    XDG_CONFIG_HOME=${XDG_CONFIG_HOME} \
+    nvim $@
 }
