07582d60d88d23cae383bca858c9fbf9bed01ad5
wuchuheng <root@wuchuheng.com>
2024-06-15 03:01:42 +0800

    feat(bin): Enhance list_docs function with exclude option
    - Update list_docs function to accept an --exclude option
    - Modify main function to include --exclude '${name}' in list_docs calls
    - Update usage example and function description to reflect the new option
    
    feat(plugin): Add dot.create.plugin function
    
    - Add src/plugins/dot/dot.plugin.zsh
    - Implement dot.create.plugin to create new plugins
    - Validate plugin name and check for existing plugins
    - Create plugin directory and sample plugin file
    - Provide success message upon plugin creation

---
+++ src/plugins/dot/dot.plugin.zsh
@@ -0,0 +1,48 @@
+import github.com/zpm-shell/zpm/src/utils/log.zsh --as log
+
+##
+# Create a new plugin.
+# @Docs: dot.create.plugin: Create a new plugin.
+##
+function dot.create.plugin() {
+    # 1. Recieve the plugin name by asking the user's input.
+    echo -n "Enter the plugin name: "
+    read -r name
+
+    # 1.1 Trim the name.
+    name=$(echo ${name} | xargs)
+
+    # 1.1 Check the name must be in a letter.
+    if [[ ! ${name} =~ ^[a-zA-Z0-9]+$ ]]; then
+        call log.error "Invalid plugin name. The plugin name must be in a letter."
+        return 1
+    fi
+
+    # 2. Check if the plugin already exists.
+    local currentDir=${G_DOTFILES_ROOT}/src/plugins/${name}
+    if [ -d ${currentDir} ]; then
+            call log.error "Plugin already exists."
+        return 1
+    fi
+
+    # 3. Create the plugin directory and the plugin file.
+    mkdir -p ${currentDir}
+    # 3.1 Create the plugin file.
+    local plugin_file=${currentDir}/${name}.plugin.zsh
+    cat <<EOF > ${plugin_file}
+##
+# This is a sample plugin.
+# @Docs: ${name}: This is a sample plugin.
+##
+function ${name}() {
+    echo "Hello, ${name}!"
+}
+
+EOF
+
+    # 4. Print the success message for the user.
+    cat ${plugin_file}
+
+    local relativeFile=${plugin_file#${G_DOTFILES_ROOT}/}
+    call log.success "Plugin: ${name} created successfully in ${relativeFile}."
+}
