2933040f38e118e12588d0e248aad50df4181a83
wuchuheng <root@wuchuheng.com>
2024-07-18 16:34:43 +0800

    feat(config): Update tmux configuration for improved session management   - Modify src/config/tmux.zsh to enhance tmux session handling and usability.

--- src/config/tmux.zsh
+++ src/config/tmux.zsh
@@ -6,4 +6,17 @@ hasTmux=${hasTmux#tmux:}
 if [[ -n ${hasTmux} ]]; then
   # Set the default terminal to tmux-256color
   set -g default-terminal "tmux-256color"
+
+  # Define your session name
+  SESSION_NAME="default"
+
+  # Check if the tmux session exists
+  if tmux has-session -t $SESSION_NAME 2>/dev/null; then
+    echo "[Tmux] Session $SESSION_NAME already exists. Attaching..."
+    tmux attach-session -t $SESSION_NAME
+  else
+    echo "[Tmux] Session $SESSION_NAME does not exist. Creating..."
+    tmux new-session -s $SESSION_NAME
+  fi
 fi
+
