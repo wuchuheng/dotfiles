d4e861daa99f83312996bdf7f4069e1e89be045d
wuchuheng <root@wuchuheng.com>
2024-08-21 04:17:56 +0800

    feat(plugin): Enhance web-vim plugin for better Neovim integration
    - Add checks for Neovim version to ensure compatibility.
    - Ensure AstroNvim is installed if not already present.
    - Create necessary directories for data, cache, state, and config.

--- src/plugins/web-vim/web-vim.plugin.zsh
+++ src/plugins/web-vim/web-vim.plugin.zsh
@@ -16,18 +16,33 @@ function wvim() {
     [[ ! -d  ${XDG_STATE_HOME} ]] && mkdir -p ${XDG_STATE_HOME}
     local XDG_CONFIG_HOME=${currentDir}/config
     [[ ! -d ${XDG_CONFIG_HOME} ]] && mkdir -p ${XDG_CONFIG_HOME}
+
+    # 1 Check if the Neovim version is less than 0.5.0, if it is, then return 1.
+    # version information like: 
+    # NVIM v0.11.0-dev-634+g169f37b94
+    local nvimVersion=$(nvim --version | head -n 1 | awk '{print $2}' | awk -F'v' '{print $2}' | awk -F'-' '{print $1}')
+    local mainVersion=$(echo ${nvimVersion} | awk -F'.' '{print $1}')
+    local secondVersion=$(echo ${nvimVersion} | awk -F'.' '{print $2}')
+    local thirdVersion=$(echo ${nvimVersion} | awk -F'.' '{print $3}')
+    if [[ ${mainVersion} -lt 0 ]] || [[ ${mainVersion} -eq 0 && ${secondVersion} -lt 5 ]]; then
+        call log.error "The current Neovim version is ${nvimVersion}, it must be greater than 0.5.0"
+        return 1
+    fi
     
-    # Check if the https://github.com/AstroNvim/AstroNvim was not installed under the path ${XDG_CONFIG_HOME}/nvim,
+    # 2. Check if the https://github.com/AstroNvim/AstroNvim was not installed under the path ${XDG_CONFIG_HOME}/nvim,
     # and then try to install it.
     local astroNvimInstalledPath=${XDG_CONFIG_HOME}/nvim
     [[ ! -d ${astroNvimInstalledPath} ]] && mkdir -p ${astroNvimInstalledPath}
-    # If the directory was empty and then clone the AstroNvim.
+    # 3. If the directory was empty and then clone the AstroNvim.
     if [[ -z "$(ls -A "${astroNvimInstalledPath}")" ]]; then
+        # 3.1 Clone the AstroNvim
         git clone --depth 1 https://github.com/wuchuheng/web-vim ${astroNvimInstalledPath}
+        # 3.1.1 If the clone was failed, then return 1.
         if [[ $? -ne 0 ]]; then
             call log.error "Failed to install AstroNvim"
             return 1
         fi
+
     fi
 
     XDG_CACHE_HOME=${XDG_CACHE_HOME} \
