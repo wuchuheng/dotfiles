43dea54c9a30575b514d3c53f1fb20d857d830d7
wuchuheng <root@wuchuheng.com>
2024-10-13 20:23:29 +0800

    feat(plugin): Add mac plugin
    - Introduce a new mac.plugin.zsh file in the src/plugins/mac directory.
    - This plugin provides functionality specific to macOS environments.
    - Initial setup and configuration for macOS users.

---
+++ src/plugins/mac/mac.plugin.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+import github.com/zpm-shell/zpm/src/utils/color.zsh --as color
+
+##
+# Disable the press and hold feature of the keyboard.
+# @Docs: mac: Disable the press and hold feature of the keyboard. usage: mac
+##
+function mac() {
+
+    # If the OS is not macOS, then return.
+    if [[ $(uname) != "Darwin" ]]; then
+        echo "This script is only supported on macOS."
+        return 1
+    fi
+
+    # Disable the press and hold feature of the keyboard.
+     defaults write -g ApplePressAndHoldEnabled -bool false
+}
+
+# Automatically run the mac function when the script is sourced.
+mac
